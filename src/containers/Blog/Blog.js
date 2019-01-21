52fab33934a00b62613f6d07a9a119221db34529
lakith <lakith1995@gmail.com>
2019-01-21 15:53:24 +0530

    add react routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,67 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import {Route} from 'react-router-dom';
+import Posts from '../Posts/Posts';
+import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
 
-    state = {
-        post : [],
-        selectedPostId : null,
-        error:false
-    }
-
-    componentDidMount(){
-        axios.get('/posts')
-            .then(
-               (res)=>{
-                   const posts = res.data.slice(0,4);
-                   const updatedPosts = posts.map(post => {
-                      return {...post,
-                                author : 'Lakith'
-                    }
-                   })
-                   this.setState({post:updatedPosts})
-                }
-            ).catch((err)=>{
-                this.setState({error:true});
-            });
-    }
-
-    postClickHandler = (id)=>{
-        this.setState({selectedPostId : id})
-    }
-
-
     render () {
-
-        let posts = <h3>Something went wrong</h3>
-        
-        if(!this.state.error){
-            posts = this.state.post
-                .map(post => (
-                    <Post 
-                        key={post.id}
-                        author={post.author}
-                        title={post.title}
-                        click={()=>this.postClickHandler(post.id)} />
-                ))
-        }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
