112c2d39d12bd57c89d01b1734dcc36cccf1e85b
lakith <lakith1995@gmail.com>
2019-01-23 21:07:57 +0530

    adding a base name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-     <BrowserRouter>
+     <BrowserRouter basename="/heliumX">
         <div className="App">
           <Blog />
         </div>
